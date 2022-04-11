aeaff88946fe7c99c99bb4ff39dc379f46d0b68d
Thomas Mailund <mailund@birc.au.dk>
2022-04-11 12:41:19 +0200

    Removed solutoins

--- src/kmer.py
+++ src/kmer.py
@@ -1,7 +1,5 @@
 """Computing kmers of a string."""
 
-from collections import Counter
-
 
 def kmer(x: str, k: int) -> list[str]:
     """
@@ -12,28 +10,22 @@ def kmer(x: str, k: int) -> list[str]:
 
     FIXME: do you want more tests here?
     """
-    return [x[i:i+k]for i in range(len(x) - k + 1)]
+    ...
 
 
 def unique_kmers(x: str, k: int) -> list[str]:
     """
     Computer all unique k-mers of x.
 
-    >>> unique_kmers('agtagtcg', 3)
-    ['agt', 'gta', 'tag', 'gtc', 'tcg']
-
     FIXME: do you want more tests here?
     """
-    return list({k: 1 for k in kmer(x, k)}.keys())
+    ...
 
 
 def count_kmers(x: str, k: int) -> dict[str, int]:
     """
     Computer all k-mers of x and count how often they appear.
 
-    >>> count_kmers('agtagtcg', 3)
-    {'agt': 2, 'gta': 1, 'tag': 1, 'gtc': 1, 'tcg': 1}
-
     FIXME: do you want more tests here?
     """
-    return dict(Counter(kmer(x, k)))
+    ...
--- src/test_kmer.py
+++ src/test_kmer.py
@@ -2,30 +2,5 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-# def test_me():
-#    assert False, "Don't forget to write tests"
-
-
-# I admit that I have been lazy with testing here, but
-# the testing part was mostly an exercise and there isn't
-# terribly much to test for these functions...
-
-from kmer import (kmer, unique_kmers, count_kmers)
-
-
-def test_kmer():
-    assert kmer('', 3) == []
-    assert kmer('agtagtcg', 3) == \
-        ['agt', 'gta', 'tag', 'agt', 'gtc', 'tcg']
-
-
-def test_unique_kmers():
-    assert unique_kmers('', 3) == []
-    assert unique_kmers('agtagtcg', 3) == \
-        ['agt', 'gta', 'tag', 'gtc', 'tcg']
-
-
-def test_count_kmers():
-    assert count_kmers('', 3) == {}
-    assert count_kmers('agtagtcg', 3) == \
-        {'agt': 2, 'gta': 1, 'tag': 1, 'gtc': 1, 'tcg': 1}
+def test_me():
+    assert False, "Don't forget to write tests"
