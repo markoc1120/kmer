d05b78d9206c176e9bcc16f97db323ab2c2a3608
markoc1120 <markoc1120@gmail.com>
2024-09-30 14:49:34 +0200

    answers

--- src/kmer.py
+++ src/kmer.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 """Computing kmers of a string."""
 
 
@@ -10,22 +12,38 @@ def kmer(x: str, k: int) -> list[str]:
 
     FIXME: do you want more tests here?
     """
-    ...
+    return [x[i : i + k] for i in range(len(x) - k + 1)]
 
 
 def unique_kmers(x: str, k: int) -> list[str]:
     """
     Computer all unique k-mers of x.
 
+    >>> unique_kmers('agtagtag', 3)
+    ['agt', 'gta', 'tag']
+
     FIXME: do you want more tests here?
     """
-    ...
+    seen, ans = set(), []
+    for i in range(len(x) - k + 1):
+        curr = x[i : i + k]
+        if curr in seen:
+            continue
+        ans.append(curr)
+        seen.add(curr)
+    return ans
 
 
 def count_kmers(x: str, k: int) -> dict[str, int]:
     """
     Computer all k-mers of x and count how often they appear.
 
+    >>> count_kmers('agtagtag', 3)
+    Counter({'agt': 2, 'gta': 2, 'tag': 2})
+
     FIXME: do you want more tests here?
     """
-    ...
+    counter = Counter()
+    for i in range(len(x) - k + 1):
+        counter[x[i : i + k]] += 1
+    return counter
--- src/test_kmer.py
+++ src/test_kmer.py
@@ -1,6 +1,17 @@
+from kmer import count_kmers, kmer, unique_kmers
+
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_me():
-    assert False, "Don't forget to write tests"
+
+def test_kmer():
+    assert kmer("agtagtcg", 3) == ["agt", "gta", "tag", "agt", "gtc", "tcg"]
+
+
+def test_unique_kmers():
+    assert unique_kmers("agtagtag", 3) == ["agt", "gta", "tag"]
+
+
+def test_count_kmers():
+    assert count_kmers("agtagtag", 3) == {"agt": 2, "gta": 2, "tag": 2}
